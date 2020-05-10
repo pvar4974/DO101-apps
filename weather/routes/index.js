756120b058149a17e3c55a8a73ea29f8d47ccd83
Aggi <paritalaagastya@outlook.com>
2020-05-10 18:47:50 -0400

    State and Country

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -12,7 +12,8 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`; 
+  /* let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`; */
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},{state},{country code}&units=${UNITS}&appid=${OWM_API_KEY}`;
   
 
   try {
